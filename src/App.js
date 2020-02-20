f0b036727d16c1fdcea13f07aad2845878de52f2
Greg Hawthorne <ghawthorne@me.com>
2020-02-20 15:37:40 -0800

    add labels

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import ReactDOM from "react-dom";
-import Pet from './Pet';
+import { render } from "react-dom";
+import SearchParams from './SearchParams';
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese"
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel"
-    }),
-    React.createElement(Pet, { name: "Moses", animal: "Cat", breed: "Mix" })
-  ]);
+  
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+      </div>
+  )
 };
+render(<App />, document.getElementById("root"));
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
